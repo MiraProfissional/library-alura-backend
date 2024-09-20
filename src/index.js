608e351fdced67ecb6cb8de876fbbc6fd45ef14e
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-19 21:16:24 -0300

    :heavy_check_mark: feat: adding the dependecy cors to the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bookRoute from './Routes/book.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: '*' }));
 
 const port = 8000;
 
